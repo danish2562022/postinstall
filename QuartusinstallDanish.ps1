2ae8aa6cf9aac1c14cb2140fa4bd3aeb1023a18d
danish2562022 <42330170+danish2562022@users.noreply.github.com>
2020-09-18 12:02:00 +0200

    Add files via upload

---
+++ QuartusinstallDanish.ps1
@@ -0,0 +1,8 @@
+function Quartus { 
+C:\Quartus\QuartusLiteSetup-18.1.0.625-windows --installdir "C:\intelFPGA_lite\18.1" --accept_eula 1
+}
+fucntion Danish { 
+C:\Quartus\Danishp.s1
+}
+Quartus 
+Danish
